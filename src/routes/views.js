335b1d05de08ec23dba0f664140fdcc6365d0139
Amelie Pira <amelie.pira@gmail.com>
2022-06-15 22:51:19 +0100

    prepared routes>views.js

---
+++ src/routes/views.js
@@ -0,0 +1,10 @@
+const { Router } = require("express");
+
+const { renderStartPage, renderNotesPage } = require("../controllers/views");
+
+const router = Router();
+
+router.get("/", renderStartPage);
+router.get("/notes", renderNotesPage);
+
+module.exports = router;
