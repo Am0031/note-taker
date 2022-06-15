9d4c0ee732d56af35622c72008956367d73ecbbc
Amelie Pira <amelie.pira@gmail.com>
2022-06-15 23:19:29 +0100

    prepared utils>interactWithData

---
+++ src/utils/interactWithDataFile.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const path = require("path");
+
+const getDataFromFile = (filepath, id) => {
+  //receives path and id (or default all)
+  //read from data file
+  //send data as response
+};
+
+const writeDataToFile = (filepath, id) => {
+  //receives path
+  //write data into file
+};
+
+module.exports = { getDataFromFile, writeDataToFile };
