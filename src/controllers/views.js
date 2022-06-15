63123543a0c7b40dd626ccec410d704b68f19297
Amelie Pira <amelie.pira@gmail.com>
2022-06-15 23:03:51 +0100

    prepared controllers>views.js

---
+++ src/controllers/views.js
@@ -0,0 +1,13 @@
+const path = require("path");
+
+const renderStartPage = () => {
+  //get file path
+  //send file as response
+};
+
+const renderNotesPage = () => {
+  //get file path
+  //send file as response
+};
+
+module.exports = { renderStartPage, renderNotesPage };
