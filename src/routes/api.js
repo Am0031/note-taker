2d6d24fbc15e2d0b6a7ff727a72a65595d5a8c6b
Amelie Pira <amelie.pira@gmail.com>
2022-06-16 18:42:53 +0100

    re-inserted extra route file (for future devs - users etc)

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,19 +1,9 @@
 const { Router } = require("express");
 
-const {
-  getNotes,
-  createNote,
-  deleteNote,
-  getNote,
-  editNote,
-} = require("../controllers/notes");
+const notes = require("./notes");
 
 const router = Router();
 
-router.get("/notes", getNotes);
-router.post("/notes", createNote);
-router.delete("/notes/:id", deleteNote);
-router.get("/notes/:id", getNote);
-router.put("/notes/:id", editNote);
+router.use("/notes", notes);
 
 module.exports = router;
