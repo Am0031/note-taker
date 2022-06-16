6dd1fa805ae93defcff2d000c33951df73191234
Amelie Pira <amelie.pira@gmail.com>
2022-06-16 15:26:35 +0100

    wrapped utils functions in try/catch

--- src/utils/interactWithDataFile.js
+++ src/utils/interactWithDataFile.js
@@ -2,19 +2,27 @@ const fs = require("fs");
 const path = require("path");
 
 const getDataFromFile = (fileName) => {
-  //get path to file
-  const filePath = path.join(__dirname, "../data", `${fileName}.json`);
-  //read from data file
-  const dataFromFile = fs.readFileSync(filePath, "utf8");
-  //send data as response
-  return JSON.parse(dataFromFile);
+  try {
+    //get path to file
+    const filePath = path.join(__dirname, "../data", `${fileName}.json`);
+    //read from data file
+    const dataFromFile = fs.readFileSync(filePath, "utf8");
+    //send data as response
+    return JSON.parse(dataFromFile);
+  } catch (error) {
+    throw error;
+  }
 };
 
 const writeDataToFile = (fileName, data) => {
-  //get path to file
-  const filePath = path.join(__dirname, "../data", `${fileName}.json`);
-  //write data into file
-  fs.writeFileSync(filePath, JSON.stringify(data));
+  try {
+    //get path to file
+    const filePath = path.join(__dirname, "../data", `${fileName}.json`);
+    //write data into file
+    fs.writeFileSync(filePath, JSON.stringify(data));
+  } catch (error) {
+    throw error;
+  }
 };
 
 module.exports = { getDataFromFile, writeDataToFile };
