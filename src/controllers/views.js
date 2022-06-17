848e9220c6cc15b63f972b759e9ef3da28145e6b
Amelie Pira <amelie.pira@gmail.com>
2022-06-17 13:56:28 +0100

    removed console.log in views controller

--- src/controllers/views.js
+++ src/controllers/views.js
@@ -1,10 +1,7 @@
 const path = require("path");
-const { getDataFromFile } = require("../utils/interactWithDataFile");
 
 const renderStartPage = (req, res) => {
   try {
-    console.log("time to render the start page");
-    //get file path
     //send file as response
     return res.sendFile(path.join(__dirname, "../../public/index.html"));
   } catch (error) {
@@ -15,11 +12,7 @@ const renderStartPage = (req, res) => {
 
 const renderNotesPage = (req, res) => {
   try {
-    console.log("time to render the notes page");
-    //get file path
-    const notes = getDataFromFile("db");
     //send file as response
-    // return res.json({ notes });
     return res.sendFile(path.join(__dirname, "../../public/notes.html"));
   } catch (error) {
     console.log("[ERROR] : Internal Server error");
